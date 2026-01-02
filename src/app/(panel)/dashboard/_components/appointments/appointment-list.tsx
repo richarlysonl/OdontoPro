cc9e1e664a4f728a37f256e5655f04b003f65e4c
richarlyson <richarluson@gmail.com>
2026-01-02 20:13:40 -0300

    aula 75 e 76 - criando grid apra exibir horarios e rota http para acessar os agendamentos com base na data informada

--- src/app/(panel)/dashboard/_components/appointments/appointment-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointment-list.tsx
@@ -1,5 +1,13 @@
 "use client"
 import { useRouter, useSearchParams } from "next/navigation"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { 
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+    CardDescription
+} from "@/components/ui/card"
 
 
 interface AppointmentsListProps{
@@ -8,11 +16,31 @@ interface AppointmentsListProps{
 export function AppointmentsList({times}:AppointmentsListProps){
     const searchParams = useSearchParams();
     const date = searchParams.get("date");
-    console.log(date)
-    console.log(times)
     return(
-        <div>
-            listagem de horarios
-        </div>
+        <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-xl md:text-2xl font-bold">Agendamentos</CardTitle>
+                <button>selecionar data</button>
+            </CardHeader>
+            <CardContent>
+                <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
+                    {times.map((slot) => {
+                        return(
+                            <div
+                        key={slot}
+                        className="flex py-2 items-center border-t last:border-b"
+                        >
+                            <div className="w-16 text-sm font-semibold px-5">
+                                {slot}
+                            </div>
+                            <div className="text-gray-500 flex-1 text-sm">
+                                disponivel
+                            </div>
+                        </div>
+                        )
+                    })}
+                </ScrollArea>
+            </CardContent>
+        </Card>
     )
 }
---
+++ src/app/api/clinic/appointments/route.ts
@@ -0,0 +1,44 @@
+import { Appointments } from "@/app/(panel)/dashboard/_components/appointments/appointments";
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { useSearchParams } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import { date } from "zod";
+/*
+    rota para buscar os agedamentos da clinica
+    => data
+    => userId (NÃO POSSO RECEBER DA ROTA URL req.params)
+*/
+export const GET = auth(async function GET(request) {
+  if (!request.auth) 
+    return NextResponse.json({ error: "acesso não autorizado" }, { status: 401 })
+    
+  const searchParams = request.nextUrl.searchParams
+  const dateString = searchParams.get("date") as string;
+  const clinicId = request.auth?.user?.id
+  if(!clinicId)
+    return NextResponse.json({error:"usuario não encontrado"}, {status:400})
+  if(!dateString)
+    return NextResponse.json({error:"data não informada"}, {status:400})
+
+    try{
+        const [year,month,day] = dateString.split("-").map(Number)
+        const startDate = new Date(year, month-1,day,0,0,0,0)
+        const endDate = new Date(year,month -1,day,23,59,59,999)
+        const appointments = await prisma.appointment.findMany({
+            where:{
+            userId:clinicId,
+            date:{
+                gte: startDate,
+                lte: endDate,
+            }
+            },
+            include:{
+                service: true
+            }
+        })
+        return NextResponse.json({appointments})
+    }catch(err){
+        return NextResponse.json({error:"falha ao buscar agendamentos"}, {status:400})
+    }
+})
