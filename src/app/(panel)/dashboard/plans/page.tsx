d616b906b575a644f8035f210530802cc137f4d4
richarlyson <richarluson@gmail.com>
2026-01-12 16:41:26 -0300

    aula 91 - busca de planos

--- src/app/(panel)/dashboard/plans/page.tsx
+++ src/app/(panel)/dashboard/plans/page.tsx
@@ -1,15 +1,22 @@
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { GridPlans } from "./_components/grid_plans";
+import { getSubscription } from "@/utils/get-subscription";
 
 export default async function Plans(){
      const session = await auth();
             console.log(session?.user?.name);
             if(!session)
                 redirect("/");
+            const subscription = await getSubscription({userId: session?.user?.id!})
     return(
         <div>
+            {subscription?.status !== "active" && (
             <GridPlans/>
+            )}
+            {subscription?.status == "active" && (
+                <h1>Você tem uma assinatura ativa</h1>
+            )}
         </div>
     )
 }
